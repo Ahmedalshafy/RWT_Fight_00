37b0849b3a0a24c916b68d81040d0cb13044a739
mahaafaisal <mahafaisalmubarak@gmail.com>
2025-06-28 11:39:26 +0400

    do

--- team_02/do.cpp
+++ team_02/do.cpp
@@ -4,6 +4,8 @@
 #include <unordered_set>
 #include <algorithm>
 #include <iostream>
+#include <iomanip>
+
 
 int countWordsThatMakeYouHappy(const std::string& sentence) {
     std::unordered_set<std::string> happyWords = {
@@ -24,13 +26,24 @@ int countWordsThatMakeYouHappy(const std::string& sentence) {
             ++count;
         }
     }
-
+    
     return count;
+}
+std::string analyzeSentiment(const std::string& sentence) {
+    int totalWords = std::count_if(sentence.begin(), sentence.end(), [](char c){ return c == ' '; }) + 1;
+    int happyCount = countWordsThatMakeYouHappy(sentence);
+    double percentage = (static_cast<double>(happyCount) / totalWords) * 100.0;
+    std::ostringstream out;
+    out << std::fixed << std::setprecision(0);
+    if (percentage >= 30.0) out << "Positive 😊 (" << percentage << "% happy words)";
+    else if (percentage >= 10.0) out << "Neutral 😐 (" << percentage << "% happy words)";
+    else out << "Negative 🙁 (" << percentage << "% happy words)";
+    return out.str();
 }
 
 int main() {
-    std::string testSentence = "I love the sunshine and feel happy today ! happiness ";
-    int result = countWordsThatMakeYouHappy(testSentence);
-    std::cout << "Found " << result << " happy words in: \"" << testSentence << "\"" << std::endl;
+    std::string s;
+    std::cin >> s;
+    std::cout << analyzeSentiment(s) << std::endl;
     return 0;
 }
